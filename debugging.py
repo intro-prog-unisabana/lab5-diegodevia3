ebd38bd1c6896f375acdc3e3c4590d98268b2a51
diego devia salas <diegodesa@unisabana.edu.co>
2026-03-16 22:35:14 -0500

    debugging2

--- debugging.py
+++ debugging.py
@@ -1,30 +1,36 @@
-def total_steps(steps):
-    return sum(steps)
-def average_steps(steps):
-    return sum(steps) // len(steps)
-def highest_steps(steps):
-    return max(steps)
-def lowest_steps(steps):
-    return min(steps)
-def goal_met(steps):
-    results = []
-    for s in steps:
-        results.append(s >= 10000)
-    return results
+steps = input("Enter steps for 7 days separated by spaces: ")
+steps_list = list(map(int, steps.split()))
 
-steps_input = input().split()
-steps = []
-for s in steps_input:
-    steps.append(int(s))
+def total_steps(data):
+    return sum(data)
 
-total=total_steps(steps)
-avg = average_steps (steps)
-highest = highest_steps(steps)
-lowest = lowest_steps(steps)
-goals = goal_met(steps)
+def average_steps(data):
+    return sum(data) // len(data)
+
+def max_steps(data):
+    return max(data)
+
+def min_steps(data):
+    return min(data)
+
+def goal_met(data):
+    result = [ ]
+    for day in data:
+        if day >= 10000:
+            result.append(True)
+        else:
+            result.append(False) 
+    return result        
+
+
+total=total_steps(steps_list)
+average = average_steps (steps_list)
+highest = max_steps(steps_list)
+lowest = min_steps(steps_list)
+goals = goal_met(steps_list)
 
 print("Total stpes:", total)
-print("Average daily steps:", avg)
-print("Highest steps in a day:", lowest)
+print("Average daily steps:", average)
+print("Highest steps in a day:", highest)
 print("Lowest steps in a day:", lowest)
 print("Goal met each day:", goals)
