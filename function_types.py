795a898d95b5b8b0617551dc02d17fff42792dff
diego devia salas <diegodesa@unisabana.edu.co>
2026-03-16 21:59:20 -0500

    modules

--- function_types.py
+++ function_types.py
@@ -1,7 +1,13 @@
-def promedio_estudiante(calificaciones):
-    if len(calificaciones) == 0:
-        return 0.0
-    suma = sum(calificaciones)
-    promedio = suma / len(calificaciones)
+def list_shift(lista, valor):
+    for i in range(len(lista)):
+        lista[i] = lista[i] + valor
+        
+def calc_avg(lista):
+    suma = 0
+    for num in lista:
+        suma += num
+    return suma / len(lista)
+
+def print_normalized(lista):
+    print(lista)
     
-    return float(promedio)
